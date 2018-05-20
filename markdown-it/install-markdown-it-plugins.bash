26325f35865ee33b0cd7f2981e5adc360961efb2
lm <lm.91@qq.com>
2018-05-20 23:05:52 +0800

    add markdown-it script to get html from markdown

---
+++ markdown-it/install-markdown-it-plugins.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File              : install-markdown-it-pulgins.bash
+# Author            : lmcallme <l.m.zhongguo@gmial.com>
+# Date              : 20.05.2018
+# Last Modified Date: 20.05.2018
+# Last Modified By  : lmcallme <l.m.zhongguo@gmial.com>
+
+sudo npm install -g markdown-it-plantuml highlight.js
